ee06b84a4aba391975b014eefce3a7927ddaa123
Devin <devin@example.com>
2024-07-05 15:47:56 +0000

    Fix relative imports in train.py

--- nonrigid_nerf/train.py
+++ nonrigid_nerf/train.py
@@ -23,10 +23,10 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
     logging.StreamHandler()
 ])
 
-from nonrigid_nerf.run_nerf_helpers import *
+from .run_nerf_helpers import *
 from scripts.text_encoder import TextEncoder
 #from load_llff import load_llff_data_multi_view
-from nonrigid_nerf.load_llff import load_llff_data
+from .load_llff import load_llff_data
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
